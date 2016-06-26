ff1648af6b02d374ce8dea4b43b07d835eaa8e44
Ian Lyttle <ian.lyttle@schneiderdashelectric.com>
2016-06-26 12:18:57 -0500

    adds more css

---
+++ R/html_doc_3.R
@@ -0,0 +1,41 @@
+#' small tweak to html_document
+#'
+#' The next simplest thing we can do is change the defaults for
+#' \code{code_folding} and theme, then pass the rest of our arguments to
+#' \code{rmarkdown::\link[rmarkdown]{html_document}}
+#'
+#' @param code_folding  Enable document readers to toggle the
+#'  display of R code chunks. We change the default to \code{"hide"}.
+#'  See \code{rmarkdown::\link[rmarkdown]{html_document}}
+#' @param theme Use to change the theme
+#' @param css One or more css files to include in addition
+#'   to this format's css.
+#' @param ... other arguments passed to
+#'   \code{rmarkdown::\link[rmarkdown]{html_document}}
+#'
+#' @return R Markdown output format to pass to
+#'   \code{rmarkdown::\link[rmarkdown]{render}}
+#'
+#' @examples
+#' \dontrun{
+#'   rmarkdown::render("foo.Rmd", html_doc_3())
+#' }
+#' @export
+#
+html_doc_3 <- function(code_folding = c("hide", "show", "none"),
+                       css = NULL, ...){
+
+  code_folding <- match.arg(code_folding)
+
+  # append supplied css to our css
+  css <- c(
+    system.file("css", "user2016format.css", package = "user2016format"),
+    css
+  )
+
+  rmarkdown::html_document(
+    code_folding = code_folding,
+    css = css,
+    ...
+  )
+}
